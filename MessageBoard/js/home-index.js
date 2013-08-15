7d0a1b3246d0f37af065b4c0b9845be1fb2052ef
Vikranth Ceakala <vceakala@conservice.com>
2013-08-15 13:00:56 -0600

    Added a angularjs controller

---
+++ MessageBoard/js/home-index.js
@@ -0,0 +1,17 @@
+﻿// hoem-index.js
+function homeIndexController($scope, $http) {
+    $scope.data = [];
+    $scope.isBusy = true;
+
+    $http.get("/api/v1/topics?includeReplies=true")
+        .then(
+            function (result) {
+                //successfull
+                angular.copy(result.data, $scope.data);
+            },
+            function () {
+                // error
+                alert("Could not load topics");
+            })
+        .then(function () { $scope.isBusy = false; });
+}
